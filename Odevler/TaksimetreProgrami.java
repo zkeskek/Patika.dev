2928ac84eadfe4ec8b1634ee1d72ba87f34f2ae4
zkeskek <trsiv580@gmail.com>
2023-01-11 23:44:35 +0300

    a

---
+++ Odevler/TaksimetreProgrami.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TaksimetreProgrami {
+    public static void main(String[] args) {
+        int km;
+        double tutar, kmFiyat=2.2;
+        System.out.println("Km bilgisini giriniz:");
+        Scanner input=new Scanner(System.in);
+        km=input.nextInt();
+        tutar=10+km*2.2;
+        if (tutar>20){
+            System.out.println("Taksimetre Tutarı:"+tutar+" TL");
+        }else{
+            System.out.println("Taksimetre Tutarı:"+20+" TL");
+        }
+    }
+}
