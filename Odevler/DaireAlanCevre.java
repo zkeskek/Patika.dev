5d56e4f2fbd7718554dc02e3b1b7937b5f7564cc
zkeskek <trsiv580@gmail.com>
2023-01-13 21:45:58 +0300

    Ödevler

---
+++ Odevler/DaireAlanCevre.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DaireAlanCevre {
+    public static void main(String[] args) {
+        int r;
+        double pi=3.14;
+        Scanner input=new Scanner(System.in);
+        r=input.nextInt();
+        System.out.println("Daire yapıçapını giriniz");
+        double cevre=2*pi*r;
+        double alan=pi*r*r;
+        System.out.println("Dairenin Çevresi: "+cevre);
+        System.out.println("Dairenin Alanı: "+alan);
+    }
+}
