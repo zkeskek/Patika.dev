c42160dc69edfb63cd96799834ecebdca2345d85
zkeskek <trsiv580@gmail.com>
2023-01-25 00:06:50 +0300

    odev

---
+++ JavaBackend/Java101/DigerKonular/SayiTahminOyunu.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * SayiTahminOyunu
+ */
+public class SayiTahminOyunu {
+
+    public static void main(String[] args) {
+        System.out.println("Sayı Tahmini oyununa hoş geldiniz.");
+        
+        Scanner input = new Scanner(System.in);
+        int sayi = (int) (Math.random() * 100);
+        int hak = 7;
+        boolean isWin = false;
+        while (true) {
+            System.out.print("0 ile 100 dahil arasında bir sayı giriniz. ");
+            int x = input.nextInt();
+            if (x < sayi) {
+                hak = hak - 1;
+                System.out.println("Sayı " + x + " sayısından büyük");
+                System.out.println(hak + " hakkınız kaldı");
+                
+            }
+            if (x == sayi) {
+                System.out.println("Tebrikler. Sayıyı buldunuz.");
+                isWin = true;
+            }
+            if (x > sayi) {
+                hak = hak - 1;
+                System.out.println("Sayı " + x + " sayısından küçük");
+                System.out.println(hak + " hakkınız kaldı");
+                
+            }
+            if (hak <= 0) {
+                
+                System.out.println("Oyunu kaybettiniz");
+                              
+
+            }
+            if(hak<=0||isWin){
+                System.out.println("Tekrar oynamak isterseniz 'E' tuşuna basınız.");
+                System.out.println("Oyundan çıkmak için herhangi bir tuşa basınız.");
+               
+                String a=input.next();
+                
+                if(a.equals("E")||a.equals("e")){
+                    sayi= (int) (Math.random() * 100);
+                    isWin=false;
+                    hak=7;
+
+                }else{
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
--- JavaBackend/Java101/Diziler/Solution1.java
+++ JavaBackend/Java101/Diziler/Solution1.java
@@ -8,24 +8,22 @@ public class Solution1 {
         int n=input.nextInt();
         input.nextLine();
         int[] array=new int[n];
-        int numberMinus=0;
+        int minusLine=0;
         int result=0;
         for(int i=0;i<n;i++){
             array[i]=input.nextInt();
         }
-        
-        for(int i=0;i<n;i++){
-            if(array[i]<0){
-                numberMinus=numberMinus+1;
-            };
-        }
-        
-        for(int i=0;i<numberMinus;i++){
-            result=result+n;
-            n=n-1;
-            
-        }
-        System.out.println(result);
+        int total=0;
+           for(int i=0;i<n;i++){
+               for(int j=i;j<n;j++){
+                   total=total+array[j];
+                   if(total<0){
+                       minusLine=minusLine+1;
+                   }
+               }
+               total=0;
+           }
+        System.out.println(minusLine);
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
